0237d2f10c879947fd08e17b4d901b46d32aae29
Jovany <jovany@ua.fm>
2016-10-24 02:07:17 +0300

    This library don't works... removed...

--- index.php
+++ index.php
@@ -9,5 +9,15 @@
 require 'vendor/autoload.php';
 
 use Joker\MakeAJoke;
+use Gregwar\Tex2png\Tex2png;
+
+
 $joker = new MakeAJoke();
-echo $joker->tellMeARandomJoke(false, "Advanced PHP", "Programmer") . "\n";
+//echo $joker->tellMeARandomJoke(false, "Advanced PHP", "Programmer") . "\n";
+
+
+
+echo "Generating sum.png...\n";
+
+Tex2png::create('\sum_{i = 0}^{i = n} \frac{i}{2}', 300)
+    ->generate();
